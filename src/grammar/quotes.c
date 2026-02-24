35641a7ff17879880e5f468129171c39dea6fe33
cuatr0-sh <cuatr0@protonmail.com>
2026-02-24 18:36:57 +0100

    solved non-whitespace separated arguments feeding as one argument

--- src/grammar/quotes.c
+++ src/grammar/quotes.c
@@ -6,7 +6,7 @@
 /*   By: asoria <asoria@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 16:55:42 by asoria            #+#    #+#             */
-/*   Updated: 2026/02/24 16:56:06 by asoria           ###   ########.fr       */
+/*   Updated: 2026/02/24 18:35:40 by asoria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ int	raw_token_len(char *s)
 	char	q;
 	int		i;
 
+	if (s[0] == '|')
+		return (1);
+	if ((s[0] == '>' || s[0] == '<') && s[1] == s[0])
+		return (2);
+	if (s[0] == '>' || s[0] == '<')
+		return (1);
 	q = 0;
 	i = 0;
 	while (s[i])
@@ -26,6 +32,8 @@ int	raw_token_len(char *s)
 		else if (q && s[i] == q)
 			q = 0;
 		else if (!q && (s[i] == ' ' || s[i] == '\t'))
+			break ;
+		else if (!q && (s[i] == '|' || s[i] == '>' || s[i] == '<'))
 			break ;
 		i++;
 	}
